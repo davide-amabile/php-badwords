f7338e3cdb0779b97ece84d13ef71c404da7a741
Unknown <davideamabile81@gmail.com>
2020-10-02 16:38:14 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$badWords = $_GET["badwords"];
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>badwords</title>
+  </head>
+  <body>
+    <h1>Siete dei <?php $badWords ?></h1>
+
+  </body>
+</html>
